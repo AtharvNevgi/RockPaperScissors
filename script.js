f9449d1c7a3429bf90fb91436bc44e560f5b46fc
AtharvNevgi <atharva.nevgi579@gmail.com>
2025-03-27 20:36:31 +0530

    new changes added in script.js

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ let paper = document.querySelector(".paper");
 let scissor = document.querySelector(".scissor");
 let userScore = document.querySelector(".userScore");
 let computerScore = document.querySelector(".computerScore");
-console.log(userScore.innerHTML);
-console.log(computerScore.innerHTML);
+
+// console.log(userScore.innerHTML);
+// console.log(computerScore.innerHTML);
+
 let options = ["rock", "paper", "scissor"];
-let computerChoice = options[Math.floor(Math.random() * options.length)]
+
 // console.log(computerChoice);
+
 rock.onclick = () => {
 	rps(rock)
 }
@@ -18,22 +21,50 @@ scissor.onclick = () => {
 	rps(scissor)
 }
 
+let user = 0;
+let computer = 0;
+
 function rps(userChoice) {
 	let userchoice = userChoice.alt;
-	// console.log(userchoice)
+	let computerChoice = options[Math.floor(Math.random() * options.length)]
+
+	console.log(userchoice)
+	console.log(computerChoice)
 
 	if(userchoice == 'rock' && computerChoice == 'paper'){
-		document.computerScore.innerHTML = computerScore += 1;	
+		computer += 1;	
+	}
+	else if(userChoice == 'rock' && computerChoice == 'scissor'){
+		user += 1;
+	}
+	else if(userChoice == 'rock' && computerChoice == 'rock'){
+		pass
+	}
+	else if(userChoice == 'paper' && computerChoice == 'rock'){
+		user += 1;
 	}
 	else if(userChoice == 'paper' && computerChoice == 'scissor'){
-		document.computerScore.innerHTML += 1;
+		computer += 1; 
+	}
+	else if(userChoice == 'paper' && computerChoice == 'paper'){
+		computer += 1; user += 1;
+	}
+	else if(userChoice == 'scissor' && computerChoice == 'paper'){
+		user += 1; 
 	}
 	else if(userChoice == 'scissor' && computerChoice == 'rock'){
-		document.computerScore.innerHTML += 1;
+		computer += 1; 
+	}
+	else if(userChoice == 'scissor' && computerChoice == 'scissor'){
+		computer += 1; user += 1;
 	}
 	else {
-		document.userScore.innerHTML += 1;
+		console.log('none')
 	}
+
+
+	console.log(user)
+	console.log(computer)
 }
 
 
